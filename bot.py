b8a1bf1809339866094bb9728a88a39094b920a3
TheMinikit <44140060+TheMinikit@users.noreply.github.com>
2020-09-30 18:51:41 +0300

    Add files via upload
    Code Refactoring

--- bot.py
+++ bot.py
@@ -7,27 +7,25 @@ import ffmpeg
 import asyncio
 import pyautogui
 from PIL import Image
-#from html.parser import HTMLParser
+
+# from html.parser import HTMLParser
 TOKEN = 'NjUzODk0NDMwNTM2NzYxMzQ0.Xe9opQ.XxkhQLgmzI_-GnNLbCyNrkZKoZg'
 
 client = discord.Client()
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\TesseractOCR\\tesseract.exe'
 
 
-#TO-DO LIST#
-#Fix Heroku Crash
-#Parse TerrariaWiki HTML Data
-#
-#
+# TO-DO#
+# Parse TerrariaWiki Data
 #
 
-
-#GENERAL#
+# GENERAL#
 async def DeleteMessageAfterDelay(message, delay):
     await asyncio.sleep(delay)
     await message.delete()
 
-#WARFRAME#
+
+# WARFRAME#
 async def GetMarketPerrinWeapons():
     MaxTops = 4
     TopBuyOrders = []
@@ -61,22 +59,24 @@ async def GetMarketPerrinWeapons():
     if len(SortedTopBuyOrders) > MaxTops:
         for i in range(MaxTops):
             ResultsString += SortedTopBuyOrders[i][0] + " buys for **" + str(int(SortedTopBuyOrders[i][1])) + "**  (" + \
-                                 SortedTopBuyOrders[i][2] + ")\n"
+                             SortedTopBuyOrders[i][2] + ")\n"
     else:
         for i in range(len(SortedTopBuyOrders)):
             ResultsString += SortedTopBuyOrders[i][0] + " buys for **" + str(int(SortedTopBuyOrders[i][1])) + "**  (" + \
-                                 SortedTopBuyOrders[i][2] + ")\n"
+                             SortedTopBuyOrders[i][2] + ")\n"
 
     ResultsString += "\n"
 
     if len(SortedTopSellOrders) > MaxTops:
         for i in range(MaxTops):
-            ResultsString += SortedTopSellOrders[i][0] + " sells for **" + str(int(SortedTopSellOrders[i][1])) + "**  (" + \
-                                 SortedTopSellOrders[i][2] + ")\n"
+            ResultsString += SortedTopSellOrders[i][0] + " sells for **" + str(
+                int(SortedTopSellOrders[i][1])) + "**  (" + \
+                             SortedTopSellOrders[i][2] + ")\n"
     else:
-        for i in range(len(SortedSellOrders)):
-            ResultsString += SortedTopSellOrders[i][0] + " sells for **" + str(int(SortedTopSellOrders[i][1])) + "**  (" + \
-                                 SortedTopSellOrders[i][2] + ")\n"
+        for i in range(len(SortedTopSellOrders)):
+            ResultsString += SortedTopSellOrders[i][0] + " sells for **" + str(
+                int(SortedTopSellOrders[i][1])) + "**  (" + \
+                             SortedTopSellOrders[i][2] + ")\n"
 
     return ResultsString
 
@@ -122,7 +122,6 @@ async def GetMarketOrders(request):
         SortedBuyOrders = sorted(BuyOrders.items(), key=lambda x: x[1], reverse=True)
         SortedSellOrders = sorted(SellOrders.items(), key=lambda x: x[1])
 
-
         if len(SortedBuyOrders) > MaxOrders:
             BuyOrdersAmount = MaxOrders
         else:
@@ -150,15 +149,16 @@ async def GetMarketOrders(request):
         Embed.add_field(name="**Sells**", value=SellString, inline=True)
         return (Embed, ResultString, (SortedBuyOrders, request), (SortedSellOrders, request))
     Embed = discord.Embed(title=request, color=0x000475)
-    return (Embed, "0", (0,"0"), (0,"0"))
+    return (Embed, "0", (0, "0"), (0, "0"))
 
-#TERRARIA#
+
+# TERRARIA#
 async def ScreenshotAndSound(message, oldString):
     myScreenshot = pyautogui.screenshot()
     myScreenshot.save("one.png")
     img = Image.open("one.png")
-    #newImg = img.crop((0,696,200,715))
-    newImg = img.crop((0,696,200,710))
+    # newImg = img.crop((0,696,200,715))
+    newImg = img.crop((0, 696, 200, 710))
     newImg.save("two.png")
     ScreenshotString = pytesseract.image_to_string(newImg)
     ScreenshotString = ScreenshotString[:-1]
@@ -169,22 +169,23 @@ async def ScreenshotAndSound(message, oldString):
             oldString = ScreenshotString
             await PlaySound(message, soundfile)
             await asyncio.sleep(5)
-            await print("Ready")
+            #await print("Ready")
             return oldString
         except:
             print("Error")
     return oldString
 
 
-#@bot.command(name="dum")
+# @bot.command(name="dum")
 async def PlaySound(message, soundfile):
     voice_channel = message.author.voice.channel
     channel = None
-    #print(client.voice_clients)
+    # print(client.voice_clients)
     if voice_channel != None:
         channel = voice_channel.name
         vc = await voice_channel.connect()
-        vc.play(discord.FFmpegPCMAudio(executable="C:/Users/XPS/Desktop/Discord/Python/ffmpeg/bin/ffmpeg.exe", source="C:/Users/XPS/Desktop/Discord/Python/Sounds/" + soundfile + ".wav"))
+        vc.play(discord.FFmpegPCMAudio(executable="C:/Users/XPS/Desktop/Discord/Python/ffmpeg/bin/ffmpeg.exe",
+                                       source="C:/Users/XPS/Desktop/Discord/Python/Sounds/" + soundfile + ".wav"))
         while vc.is_playing():
             await asyncio.sleep(.5)
         await vc.disconnect()
@@ -198,104 +199,21 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     print("")
 
+
 @client.event
 async def on_message(message):
-    if message.content == "wf invasions":
-        Req = requests.get('https://api.warframestat.us/pc/invasions')
-        Content = Req.content
-        RewardsList = []
-        RewardsString = ""
-        Reward = " "
-        OuterList = json.loads(Content)
-        for Item in OuterList:
-            if Item["completed"] == False:
-                RewardsString += str(Item["node"]) + "  " + str(round(Item["completion"], 1)) + "%\n"
-                try:
-                    RewardData = Item["attackerReward"]["countedItems"][0]
-                    if RewardData["count"] > 1:
-                        RewardsString += str(RewardData["count"]) + " x " + str(RewardData["key"])
-                    else:
-                        RewardsString += str(RewardData["key"])
 
-                    RewardData = Item["defenderReward"]["countedItems"][0]
-                    RewardsString += " and "
-
-                    if RewardData["count"] > 1:
-                        RewardsString += str(RewardData["count"]) + " x " + str(RewardData["key"])
-                    else:
-                        RewardsString += str(RewardData["key"])
-                    RewardsString += "\n"
-
-                except:
-                    RewardData = Item["defenderReward"]["countedItems"][0]
-                    if RewardData["count"] > 1:
-                        RewardsString += str(RewardData["count"]) + " x " + str(RewardData["key"])
-                    else:
-                        RewardsString += str(RewardData["key"])
-                    RewardsString += "\n"
-                RewardsString += "\n"
-
-        if "Orokin Reactor Blueprint" in RewardsString or "Orokin Catalyst Blueprint" in RewardsString:
-            Role = client.guild.roles.mention('name', 'Warframe')
-            message.channel.send(Role.mention() + " Orokin stuff detected.")
-
-        await DeleteMessageAfterDelay(message, 0.5)
-        await DeleteMessageAfterDelay(await message.channel.send(RewardsString), 30)
-
-    if message.content == "wf cycles":
-        ResultString = ""
-
-        Req = requests.get('https://api.warframestat.us/pc/cetusCycle')
-        Content = Req.content
-        CetusData = json.loads(Content)
-        if CetusData["state"] == "day" or CetusData["state"] == "night":
-            ResultString += "Cetus: " + CetusData["state"].capitalize() + " (" + CetusData["shortString"] + ")\n\n"
-        else:
-            ResultString += "Cetus Cycle Error."
-
-        Req = requests.get('https://api.warframestat.us/pc/vallisCycle')
-        Content = Req.content
-        VallisData = json.loads(Content)
-        if VallisData["state"] == "cold" or VallisData["state"] == "warm":
-            ResultString += "Orb Vallis: " + VallisData["state"].capitalize() + " (" + VallisData["shortString"] + ")\n\n"
-        else:
-            ResultString += "Orb Vallis Cycle Error."
-
-        Req = requests.get('https://api.warframestat.us/pc/cambionCycle')
-        Content = Req.content
-        CambionData = json.loads(Content)
-        if CambionData["active"] == "fass":
-            ResultString += "Cambion Drift: " + CambionData["active"].capitalize() + " (??m to Vome)"
-        elif CambionData["active"] == "vome":
-            ResultString += "Cambion Drift: " + CambionData["active"].capitalize() + " (??m to Fass)"
-        else:
-            ResultString += "Cambion Drift Cycle Error."
-
-        await DeleteMessageAfterDelay(message, 0.5)
-        await DeleteMessageAfterDelay(await message.channel.send(ResultString), 10)
-
-
-
-    if message.content == "wf perrin":
-        ResultsString = await GetMarketPerrinWeapons()
-
-        await DeleteMessageAfterDelay(message, 0.5)
-        await DeleteMessageAfterDelay(await message.channel.send(ResultsString), 10)
-
-
-    if message.content == "TERRARIA":
+    if message.content == "saysounds":
         oldString = ""
-        while(True):
+        while (True):
             if oldString == "<Kit> goodnightbot":
                 break
             oldString = await ScreenshotAndSound(message, oldString)
             await asyncio.sleep(.25)
 
-
     if message.content == "goodnight":
         await message.delete()
         await client.logout()
-
 
     if message.content == "terra":
         Req = requests.get('https://terraria.gamepedia.com/api.php?action=parse&format=json&page=Titanium_Crate')
@@ -305,31 +223,36 @@ async def on_message(message):
         Reward = " "
         PageData = json.loads(Content)
         ParseData = PageData["parse"]["text"]["*"]
-        #ParsedData = HTMLParser.feed(ParseData)
-        print(ParsedData)
+        # ParsedData = HTMLParser.feed(ParseData)
+        #print(ParsedData)
         await message.delete()
 
-
-    if message.content == "bot help":
-
-        await DeleteMessageAfterDelay(await message.channel.send("General:\n"
-                                   "bot help : Get all available bot commands\n"
-                                   "\n"
-                                   "Warframe:\n"
-                                   "wf invasions : Get Invasions Data \n"
-                                   "wf cycles : Get World Cycles Data \n"
-                                   "wf perrin : Get Perrin Sequence Weapons Buy/Sell Orders from Warframe Market \n"
-                                   "wf market <weapon> : Get Weapon Buy/Sell Orders from Warframe Market\n"
-                                   "\n"
-                                   "Local Use Only:\n"
-                                   "p list : Get all available sounds to play\n"
-                                   "p <sound> : Play sound in your voice channel"), 30)
-        await DeleteMessageAfterDelay(message, 0.5)
-
+    if message.content == "test":
+        print("")
 
     if len(message.content.split()) > 1:
 
-        if message.content.split()[0] == "p":       #Double Word(Parameter) Commands (Example: p list)
+        if message.content.split()[0] == "bot":
+            if message.content.split()[1] == "help":
+                await DeleteMessageAfterDelay(message, 0.5)
+                await DeleteMessageAfterDelay(await message.channel.send("General:\n"
+                                                                         "bot help : Get all available bot commands\n"
+                                                                         "\n"
+                                                                         "Warframe:\n"
+                                                                         "wf invasions : Get Invasions Data \n"
+                                                                         "wf cycles : Get World Cycles Data \n"
+                                                                         "wf perrin : Get Perrin Sequence Weapons Buy/Sell Orders from Warframe Market \n"
+                                                                         "wf market <weapon> : Get Weapon Buy/Sell Orders from Warframe Market\n"
+                                                                         "\n"
+                                                                         "Local Use Only:\n"
+                                                                         "p list : Get all available sounds to play\n"
+                                                                         "p <sound> : Play sound in your voice channel"),
+                                              30)
+            else:
+                await DeleteMessageAfterDelay(await message.channel.send("No such Bot Command.\n"
+                                                                         "Check bot help for available commands."), 5)
+
+        elif message.content.split()[0] == "p":
             if message.content.split()[1] == "list":
                 FileList = os.listdir("C:/Users/XPS/Desktop/Discord/Python/Sounds")
                 FileListString = ""
@@ -341,32 +264,96 @@ async def on_message(message):
                 await PlaySound(message, soundfile)
                 await DeleteMessageAfterDelay(message, 0.5)
 
+        elif message.content.split()[0] == "wf":
+            if message.content.split()[1] == "invasions":
+                Req = requests.get('https://api.warframestat.us/pc/invasions')
+                Content = Req.content
+                RewardsList = []
+                RewardsString = ""
+                Reward = " "
+                OuterList = json.loads(Content)
+                for Item in OuterList:
+                    if Item["completed"] == False:
+                        RewardsString += str(Item["node"]) + "  " + str(round(Item["completion"], 1)) + "%\n"
+                        try:
+                            RewardData = Item["attackerReward"]["countedItems"][0]
+                            if RewardData["count"] > 1:
+                                RewardsString += str(RewardData["count"]) + " x " + str(RewardData["key"])
+                            else:
+                                RewardsString += str(RewardData["key"])
 
-    if len(message.content.split()) > 2:        #Triple Word(Parameter) Commands (Example: wf market secura_lecta)
+                                RewardData = Item["defenderReward"]["countedItems"][0]
+                                RewardsString += " and "
 
-        if message.content.split()[0] == "wf" and message.content.split()[1] == "market":
-            await DeleteMessageAfterDelay(message, 0.5)
-            try:
-                Embed, ReturnString, Buys, Sells = await GetMarketOrders(message.content.split()[2])
-                if ReturnString != "0":
-                    await DeleteMessageAfterDelay(await message.channel.send(embed=Embed), 30)
-            except:
+                            if RewardData["count"] > 1:
+                                RewardsString += str(RewardData["count"]) + " x " + str(RewardData["key"])
+                            else:
+                                RewardsString += str(RewardData["key"])
+                            RewardsString += "\n"
 
-                await DeleteMessageAfterDelay(await message.channel.send("Error!"), 10)
+                        except:
+                            RewardData = Item["defenderReward"]["countedItems"][0]
+                            if RewardData["count"] > 1:
+                                RewardsString += str(RewardData["count"]) + " x " + str(RewardData["key"])
+                            else:
+                                RewardsString += str(RewardData["key"])
+                            RewardsString += "\n"
+                        RewardsString += "\n"
 
+                if "Orokin Reactor Blueprint" in RewardsString or "Orokin Catalyst Blueprint" in RewardsString:
+                    message.channel.send("<@&749224566022471690>" + " Orokin stuff detected.")
 
+                await DeleteMessageAfterDelay(message, 0.5)
+                await DeleteMessageAfterDelay(await message.channel.send(RewardsString), 30)
+            elif message.content.split()[1] == "cycles":
+                ResultString = ""
+                Req = requests.get('https://api.warframestat.us/pc/cetusCycle')
+                Content = Req.content
+                CetusData = json.loads(Content)
+                if CetusData["state"] == "day" or CetusData["state"] == "night":
+                    ResultString += "Cetus: " + CetusData["state"].capitalize() + " (" + CetusData[
+                    "shortString"] + ")\n\n"
+                else:
+                    ResultString += "Cetus Cycle Error."
 
-    if len(message.content.split()) > 2:
+                Req = requests.get('https://api.warframestat.us/pc/vallisCycle')
+                Content = Req.content
+                VallisData = json.loads(Content)
+                if VallisData["state"] == "cold" or VallisData["state"] == "warm":
+                    ResultString += "Orb Vallis: " + VallisData["state"].capitalize() + " (" + VallisData[
+                    "shortString"] + ")\n\n"
+                else:
+                    ResultString += "Orb Vallis Cycle Error."
 
-        if message.content.split()[0] == "wf" and message.content.split()[1] == "market":
-            try:
-                Embed, ReturnString, Buys, Sells = await GetMarketOrders(message.content.split()[2])
-                if ReturnString != "0":
-                    await message.channel.send(embed=Embed)
-            except:
-                await message.channel.send("Error!")
+                Req = requests.get('https://api.warframestat.us/pc/cambionCycle')
+                Content = Req.content
+                CambionData = json.loads(Content)
+                if CambionData["active"] == "fass":
+                    ResultString += "Cambion Drift: " + CambionData["active"].capitalize() + " (??m to Vome)"
+                elif CambionData["active"] == "vome":
+                    ResultString += "Cambion Drift: " + CambionData["active"].capitalize() + " (??m to Fass)"
+                else:
+                    ResultString += "Cambion Drift Cycle Error."
 
-
-
+                await DeleteMessageAfterDelay(message, 0.5)
+                await DeleteMessageAfterDelay(await message.channel.send(ResultString), 10)
+            elif message.content.split()[1] == "perrin":
+                ResultsString = await GetMarketPerrinWeapons()
+                await DeleteMessageAfterDelay(message, 0.5)
+                await DeleteMessageAfterDelay(await message.channel.send(ResultsString), 10)
+            elif message.content.split()[1] == "market":
+                if len(message.content.split()) > 2:
+                    await DeleteMessageAfterDelay(message, 0.5)
+                    try:
+                        Embed, ReturnString, Buys, Sells = await GetMarketOrders(message.content.split()[2])
+                        if ReturnString != "0":
+                            await DeleteMessageAfterDelay(await message.channel.send(embed=Embed), 30)
+                    except:
+                        await DeleteMessageAfterDelay(await message.channel.send("Error!"), 10)
+                else:
+                    await DeleteMessageAfterDelay(await message.channel.send("Error! Please use format: wf market <item>"), 10)
+            else:
+                await DeleteMessageAfterDelay(await message.channel.send("No such Warframe command.\n"
+                                                                         "Check bot help for available commands"), 5)
 
 client.run(TOKEN)
